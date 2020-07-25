5676c9a3e9e86e1a5a1d579b43ac051526f356d3
Francisco Silva <ischawk@gmail.com>
2020-07-25 11:09:26 -0500

    Validating and testing last version of emails for new customers

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 
 module.exports = {
+
     newCustomer: function (email) {
         console.log("Entro a enviar correo");
         var transporter = nodemailer.createTransport({
